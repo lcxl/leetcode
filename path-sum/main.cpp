3796a027cea4be1bdcba11b1e1c636585849d76d
luochx <luochx@yonyou.com>
2015-09-25 21:52:30 +0800

    完成path-sum

---
+++ path-sum/main.cpp
@@ -0,0 +1,50 @@
+#include<vector>
+#include<cstdio>
+#include<string>
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+
+ struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	bool hasPathSum(TreeNode* root, int sum) {
+		if (!root) {
+			return false;
+		}
+		int nextsum = sum - root->val;
+		if (root->left) {
+			if (hasPathSum(root->left, nextsum)) {
+				return true;
+			}
+		}
+		if (root->right) {
+			if (hasPathSum(root->right, nextsum)) {
+				return true;
+			}
+		}
+		if (!root->left && !root->right) {
+			return nextsum == 0;
+		}
+		return false;
+	}
+};
+
+int main()
+{
+	Solution solution = Solution();
+
+	return 0;
+}
